4bd6e0c3e99369f2121524f5439f44990bb68533
Daniel Martinez <zkdiff@gmail.com>
2022-05-23 13:23:50 -0700

    basic remix template

---
+++ app/entry.server.tsx
@@ -0,0 +1,15 @@
+import { renderToString } from "react-dom/server";
+import { RemixServer } from "@remix-run/react";
+
+function handleRequest(request, responseStatusCode, responseHeaders, remixContext) {
+  const markup = renderToString(<RemixServer context={remixContext} url={request.url} />);
+
+  responseHeaders.set("Content-Type", "text/html");
+
+  return new Response("<!DOCTYPE html>" + markup, {
+    status: responseStatusCode,
+    headers: responseHeaders,
+  });
+}
+
+export default handleRequest;
---
+++ app/root.tsx
@@ -0,0 +1,26 @@
+import * as React from "react";
+import { Scripts } from "@remix-run/react";
+
+function App() {
+  const [count, setCount] = React.useState(0);
+  return (
+    <html>
+      <head>
+        <title>My First Remix App</title>
+      </head>
+      <body>
+        <p>This is a remix app. Hooray!</p>
+        <button
+          onClick={() => {
+            setCount((c) => c + 1);
+          }}
+        >
+          {count}
+        </button>
+        <Scripts />
+      </body>
+    </html>
+  );
+}
+
+export default App;
