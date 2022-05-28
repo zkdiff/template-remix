5510fd5a577e635e5c87e4d186d0bec7de356475
Daniel Martinez <zkdiff@gmail.com>
2022-05-27 20:21:49 -0700

    remove red screen

--- app/routes/__index.tsx
+++ app/routes/__index.tsx
@@ -1,9 +1,5 @@
 import { Outlet } from "@remix-run/react";
 
 export default function IndexPage() {
-  return (
-    <div className="h-screen bg-red-500">
-      <Outlet />
-    </div>
-  );
+  return <Outlet />;
 }
